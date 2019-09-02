bd4fcfe6d568f7559241492164aa329eb9b7a6f9
linzhongqi <121351011@qq.com>
2019-09-02 19:18:00 +0800

    init

---
+++ src/components/seek.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="seekBox">
+    <div class="seekTop">
+      <img src="../../static/images/b.png" alt class="bBox" />
+      <input type="text" class="seekInp" placeholder="想吃啥包子？" />
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.seekBox {
+  width: 70%;
+  height: 100%;
+  .seekTop {
+    height: 100%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    width: 100%;
+    .bBox {
+      width: 30px;
+      height: 30px;
+      vertical-align: middle;
+      margin-right: 2px;
+    }
+    .seekInp {
+      width: 75%;
+      height: 30px;
+      border-radius: 15px;
+      background: #fff;
+      padding-left: 15px;
+      font-size: 13px;
+    }
+  }
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="homeBox">
+    <div class="header">
+      <Seek></Seek>
+    </div>
+    <div class="body">
+      <div class="bodyH"></div>
+      <div class="bodyB"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Seek from '../../components/seek'
+
+export default {
+  data () {
+    return {}
+  },
+
+  components: {
+    Seek
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+.homeBox {
+  width: 100%;
+  height: 100vh;
+  background-color: #ffcc00;
+  .header {
+    height: 82px;
+    margin-bottom: 20px;
+  }
+  .body {
+    width: 95%;
+    margin: auto;
+    .bodyH {
+      width: 90%;
+      margin: auto;
+      background: #fff;
+      height: 50px;
+      border-radius: 20px 20px 0 0;
+      box-shadow: 0px 9px 10px 0px #999 ;
+    }
+    .bodyB {
+      width: 100%;
+      height: 250px;
+      background: #fff;
+      border-radius: 20px 20px 0 0;
+      box-shadow: 0px 9px 10px 0px #999 ;
+    }
+  }
+}
+</style>
